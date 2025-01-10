ffba72b6d14215f60043520e23875091663dce39
guilhermegnc <ggnchaves@yahoo.com>
2025-01-10 17:27:51 -0300

    Adding jQuery plugin for tags

--- sceneSetup.js
+++ sceneSetup.js
@@ -13,6 +13,8 @@ function setupScene(vertexShader, fragmentShader) {
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
+    // Add an ID to the canvas element
+    renderer.domElement.id = "threeCanvas";
     document.body.appendChild(renderer.domElement);
 
     // Grain Texture
--- script.js
+++ script.js
@@ -2,3 +2,39 @@ import { loadShaders } from './sceneSetup.js';
 
 loadShaders();
 
+$(document).ready(function() {
+    if(!$("#myCanvas").tagcanvas({
+        textColour: "#ffffff",
+        outlineColour: "transparent",
+        reverse: true,
+        depth: 0.8,
+        maxSpeed: 0.05,
+        weigth: true
+    }, "knowledgeList")){
+        $("canvasContainer");
+    }
+})
+
+const canvas = document.getElementById('myCanvas'); // Get your canvas element
+
+function resizeCanvas() {
+    const dpr = window.devicePixelRatio || 1;  // Get the device pixel ratio (default to 1 if not available)
+
+    // Get the viewport dimensions
+    const viewportWidth = window.innerWidth *0.4;
+    const viewportHeight = window.innerHeight *0.7;
+
+    // Set the canvas width and height in pixels
+    canvas.width = viewportWidth * dpr;  // Multiply by dpr for high-DPI scaling
+    canvas.height = viewportHeight * dpr;  // Multiply by dpr for high-DPI scaling
+
+    // Optionally, scale the canvas context to match device pixel ratio
+    const ctx = canvas.getContext('2d');
+    ctx.scale(dpr, dpr);
+}
+
+// Initial resize
+resizeCanvas();
+
+// Update canvas size when the window is resized
+window.addEventListener('resize', resizeCanvas);
